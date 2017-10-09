25e363488cf13b904aaf0ee9bb13a05aa1cc539d
kubaorlowski <orlowskijakub1@gmail.com>
2017-10-09 15:02:13 +0200

    cw3.1

--- cw3.1/main.cpp
+++ cw3.1/main.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <stdexcept>
+#include <string>
+
 
 using namespace std;
 
-int main()
+    struct Probka
+    {
+        double t;
+        double x;
+        Probka(double _t,double _x)
+        {
+            t=_t;
+            x=_x;
+        }
+    };
+
+    vector <Probka> wczytaj (string abc)
+    {
+      vector <Probka> tablica;
+      ifstream plik (abc);
+      string linia;
+      while (getline(plik,linia))
+      {
+          cout << linia << endl;
+      };
+      plik close;
+      return tablica;
+    }
+
+    //tablica push_back (probka1);
+    //cout<<tablica[a].t<<endl;
+
+int main(int argc, char* argv[])
 {
-    cout << "Hello world!" << endl;
+
+    if (argc !=2)
+    {
+        return -1;
+    }
+
+    vector <Probka> dane=wczytaj(argv[1]);
+
+
+
+
+
+
     return 0;
 }
