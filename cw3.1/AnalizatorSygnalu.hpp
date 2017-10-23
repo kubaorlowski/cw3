a8d7cf574262b669987c2ea7fc4db9b82f71aaf8
kubaorlowski <32266605+kubaorlowski@users.noreply.github.com>
2017-10-23 15:30:20 +0200

    cw4

---
+++ cw3.1/AnalizatorSygnalu.hpp
@@ -0,0 +1,15 @@
+#ifndef ANALIZATORSYGNALU_HPP_INCLUDED
+#define ANALIZATORSYGNALU_HPP_INCLUDED
+
+class AnalizatorSygnalu
+{
+public:
+    AnalizatorSygnalu();
+    double dlugosc(sygnal:const Sygnal&);
+    double minimum(sygnal:const Sygnal&);
+    double srednia(sygnal:const Sygnal&);
+    double calka(sygnal:const Sygnal&);
+};
+
+
+#endif // ANALIZATORSYGNALU_HPP_INCLUDED
